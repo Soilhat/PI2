4eb365b86a219e76f2123c3e1646fa0556f1add2
anja1612 <anja.raharijaona@devinci.fr>
2019-03-26 12:34:31 +0100

    GUI

---
+++ src/sample/Model/LittleMoveController.java
@@ -0,0 +1,72 @@
+package Model;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import service.move.impl.LittleMoveServiceImpl;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+
+public class LittleMoveController implements Initializable {
+    private LittleMoveServiceImpl data =new LittleMoveServiceImpl();
+    private int threshold;
+
+    private List<MoveDuration> datas;
+    public int getThreshold() {
+        return threshold;
+    }
+
+    @FXML
+    TextArea outputAverage;
+    @FXML
+    TextArea outputDureeMoyenne;
+    @FXML
+    TextField thresholdText;
+
+    @FXML
+    DatePicker datePicker;
+    @FXML
+    TextField printDate;
+    @FXML
+    Button btnDate;
+
+    @FXML
+    Button BtnList;
+
+    @FXML
+    ListView listLittleMove;
+
+    @FXML
+    Button duration;
+
+    @FXML
+    TextArea outputDureeTotal;
+
+    @FXML
+    Button moyenne;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+    }
+
+    public void afficherListDeplacement(){
+        String  thresholdString = thresholdText.getText();
+        threshold = Integer.parseInt(thresholdString);
+        datas  = data.littleMoveDuration(threshold);
+        listLittleMove.setItems(FXCollections.observableList(datas));
+    }
+
+    public void afficherDureeTotal(){
+        outputDureeTotal.appendText(data.littleMoveTotalDurationFormatted(datas));
+    }
+
+    public void afficherDureeMoyenne(){
+
+        outputAverage.appendText(data.littleMoveAverageFormatted(datas));
+    }
+
+}
